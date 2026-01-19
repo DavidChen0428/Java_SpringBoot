9d65bf3f31e34c5204afd37291cd0416f753c819
DavidChen0428 <davidchen428@yahoo.com>
2026-01-19 23:00:42 +0800

    add simple log4j2-example

---
+++ log4j2-example/src/test/java/com/david/project/Log4j2ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Log4j2ExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
