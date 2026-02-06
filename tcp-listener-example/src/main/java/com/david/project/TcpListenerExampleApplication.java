ff86d523e5711c21ff68b931b121e1c56e14e3bd
DavidChen0428 <davidchen428@yahoo.com>
2026-02-06 22:58:58 +0800

    add simple tcp-listener-example

---
+++ tcp-listener-example/src/main/java/com/david/project/TcpListenerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.david.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcpListenerExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcpListenerExampleApplication.class, args);
+    }
+
+}
